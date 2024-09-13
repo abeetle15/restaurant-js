8e99d972e0b5c831254a1ade4bdb3bcc25bba656
A Beetle <abeetle15@gmail.com>
2024-09-13 11:52:03 -0500

    Home page setup
    add html directly to template (this will change). set up global css
    settings, fonts, etc.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import "./styles.css";
+import "./reset.css";
 
+const contentDiv = document.querySelector('#content');
+
+contentDiv.innerHTML = HomeNav();
+
+function HomeNav() {
+  return `
+      <h2 id="welcome-sub">La mejor arepa de chócolo de Bogotá</h2>
+
+    <nav id="welcome-nav">
+      <button class="btn">Nuesta carta</button>
+      <button class="btn">Sobre nosotros</button>
+      <button class="btn">Contacto</button>
+    </nav>
+  `
+}
